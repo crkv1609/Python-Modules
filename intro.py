579878aefa4ca688678ed181a94a5bce47ccbe7e
cyber <rkv.16996@gmail.com>
2024-07-04 11:34:42 +0530

    learning python

---
+++ intro.py
@@ -0,0 +1,14 @@
+# print stmt with welcome msg
+# print("Welcome to Python Charm Editor")
+
+"""
+msg = "Welcome to Python Charm Editor" # Literal
+print(msg)
+"""
+
+""""
+msg = input('Please enter msg')
+print(msg)
+"""
+
+
